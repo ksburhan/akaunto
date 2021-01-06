fb36a65a8641f1bc586f3d87f01ef12047aca80b
ksburhan <burhan.koeseler@hotmail.de>
2021-01-06 21:21:37 +0100

    can write posts that get saved, can see own posts in home screen

--- src/main/java/de/othr/sw/ksburhan/akaunto/controller/HomeController.java
+++ src/main/java/de/othr/sw/ksburhan/akaunto/controller/HomeController.java
@@ -1,8 +1,12 @@
 package de.othr.sw.ksburhan.akaunto.controller;
 
 import de.othr.sw.ksburhan.akaunto.entity.Account;
+import de.othr.sw.ksburhan.akaunto.entity.CustomAccount;
+import de.othr.sw.ksburhan.akaunto.entity.Post;
 import de.othr.sw.ksburhan.akaunto.service.AccountService;
+import de.othr.sw.ksburhan.akaunto.service.TimelineService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +21,9 @@ public class HomeController {
 
     @Autowired
     private AccountService accountService;
+
+    @Autowired
+    private TimelineService timelineService;
 
     @GetMapping("/")
     public String showStartPage(Model model) {
@@ -54,8 +61,23 @@ public class HomeController {
     }
 
     @RequestMapping("/home")
-    public String showHomescreen(Account account, Model model) {
+    public String showHomescreen(@AuthenticationPrincipal CustomAccount customAccount, Model model) {
+
+        Account account = accountService.findByUsername(customAccount.getUsername());
+        List<Post> allPosts = timelineService.findByAuthorID(account.getId());
+        model.addAttribute("allPosts", allPosts);
         model.addAttribute("account", account);
+        model.addAttribute("post", new Post());
+        return "home";
+    }
+
+    @PostMapping("/post")
+    public String createNewPost(@AuthenticationPrincipal CustomAccount customAccount, Post post, Model model) {
+        post.setAuthorID(accountService.findByUsername(customAccount.getUsername()).getId());
+        post.setCurrentDate();
+
+        timelineService.save(post);
+
         return "home";
     }
 }
--- src/main/java/de/othr/sw/ksburhan/akaunto/service/TimelineService.java
+++ src/main/java/de/othr/sw/ksburhan/akaunto/service/TimelineService.java
@@ -4,9 +4,11 @@ import de.othr.sw.ksburhan.akaunto.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface TimelineService extends JpaRepository<Post, Long> {
 
     @Query("SELECT p FROM Post p WHERE p.authorID = ?1")
-    public Post findByAuthorID(Long authorId);
+    public List<Post> findByAuthorID(Long authorId);
 
 }
