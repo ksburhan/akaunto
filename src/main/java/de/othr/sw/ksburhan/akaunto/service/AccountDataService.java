3893def6dfcef9ed020452df0f52b7f29568602c
ksburhan <burhan.koeseler@hotmail.de>
2021-01-25 16:21:57 +0100

    created function for rest call

--- src/main/java/de/othr/sw/ksburhan/akaunto/service/AccountDataService.java
+++ src/main/java/de/othr/sw/ksburhan/akaunto/service/AccountDataService.java
@@ -3,6 +3,7 @@ package de.othr.sw.ksburhan.akaunto.service;
 import de.othr.sw.ksburhan.akaunto.repository.AccountDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class AccountDataService {
@@ -12,6 +13,15 @@ public class AccountDataService {
     @Autowired
     public AccountDataService(AccountDataRepository accountDataRepository) {
         this.accountDataRepository = accountDataRepository;
+    }
+
+    public void getAccountDataFromBeachCourts(String username){
+        final String url = "http://localhost:8083/api/v1/user/advertisingInformation?username=" + username;
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(url, String.class);
+
+        System.out.println(result);
     }
 
 }
